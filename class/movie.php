c3f402b01326f333d0960c9c387451eb757d840d
SaverioCascelli <saverio.cascelli@gmail.com>
2022-12-07 16:54:30 +0100

    [add] class complete

--- class/movie.php
+++ class/movie.php
@@ -3,13 +3,21 @@
 class Movie
 {
     public $name;
-    public $genre = [];
+    public $genre;
     public $imgUrl;
-
-    function __construct($name, array $genre, $imgUrl)
+    public $rating;
+    public function __construct($_name, $_genre, $_rating, $_imgUrl = null)
     {
-        $this->name = $name;
-        $this->genre = $genre;
-        $this->imgUrl = $imgUrl;
+        $imgPlaceholder = 'https://media.istockphoto.com/id/1147544807/it/vettoriale/la-commissione-per-la-immagine-di-anteprima-grafica-vettoriale.jpg?s=612x612&w=0&k=20&c=gsxHNYV71DzPuhyg-btvo-QhhTwWY0z4SGCSe44rvg4=';
+        $this->name = $_name;
+        $this->genre = $_genre;
+        $this->imgUrl = $_imgUrl ?? $imgPlaceholder;
+        $this->rating = $_rating;
+    }
+
+    public function getStars()
+    {
+        $stars = $this->rating;
+        return ceil($stars / 2);
     }
 }
--- index.php
+++ index.php
@@ -6,7 +6,20 @@ include_once './partials/head.php';
 ?>
 
 <body>
+    <?php
+    include './class/movie.php';
+    $arr = [
+        'romantico', 'azione'
+    ];
 
+    $test = new Movie('asdf', 'romantico', 10, 'ilmiourl');
+    $test2 = new Movie('asdf', 'romantico', 5);
+
+    var_dump($test);
+    var_dump($test2);
+    echo $test->getStars();
+    echo $test2->getStars();
+    ?>
 </body>
 
 </html>
